a2a987dbbc8a0bda8e6590d8a7bcb6d5d3c3545e
codewdme <lll.rg3.lll@gmail.com>
2024-04-22 13:19:55 +0530

    Redux Toolkit to do app

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
